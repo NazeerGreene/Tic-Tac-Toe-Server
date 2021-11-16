9ff5aa2fc3aa091d56f51acf964231d1d5a2776d
Nazeer Greene <58005647+NazeerGreene@users.noreply.github.com>
2021-11-15 22:45:56 -0800

    Separate Game and Server
    1. ttt_client and ttt_server are the networking files
    2. TTT.cpp TTT.hpp and session.cpp are the game files.
    3. Now I need to combine the two aspects into a single game server, serving a single client.

--- TTT.cpp
+++ TTT.cpp
@@ -1,386 +1,270 @@
-//TTT client
-//IPv4 only
+#include "TTT.hpp"
 
-#include <iostream>
-#include <array>
-#include <string>
-#include <random>
-#include "helpers.h"
 
-#define TTT_SERVER_PORT         "6767"
+void TTT_default_board_decorators(std::string& player_char, std::string& opp_char, std::string& empty_space)
+{
+    player_char = DEFAULT_PLAYER_CHAR;
+    opp_char    = DEFAULT_OPPONENT_CHAR;
+    empty_space = DEFAULT_EMPTY_CHAR;
+}
 
-#define DEFAULT_PLAYER_CHAR     "X"
-#define DEFAULT_OPPONENT_CHAR   "O"
-#define DEFAULT_EMPTY_CHAR      "~"
-#define BOARD_INDEX_OFFSET       1
-//BOARD_INDEX_OFFSET is necessary to adjust user input
-//into the proper array index
-//user options [1...9] - BOARD_INDEX_OFFSET --> indexing [0..8]
+void TTT_verify_board_decorators(std::string& player, std::string& opponent, std::string& empty_space) 
+{
+    if (player.length() != 1 || 
+        player == DEFAULT_OPPONENT_CHAR ||
+        player == DEFAULT_EMPTY_CHAR) 
 
-#define ERROR   -1
-#define SUCCESS  0
+        player = DEFAULT_PLAYER_CHAR;
+    
 
-class TTT {
-    //Basic 3x3 tic tac toe game; not scalable
-    //Spaces enumerated from [0...8]
-    //board characters refer to player, opponent, and empty space
-    public:
+    if (opponent.length() != 1 || 
+        opponent == player || 
+        opponent == DEFAULT_PLAYER_CHAR || 
+        opponent == DEFAULT_EMPTY_CHAR)
 
-    //default constructor
-    TTT()
-        : points(0), server_fd(0)
-    {
-        player_char = DEFAULT_PLAYER_CHAR;
-        opp_char    = DEFAULT_EMPTY_CHAR;
+        opponent = DEFAULT_OPPONENT_CHAR;
+    
+    
+    if (empty_space.length() != 1 || 
+        empty_space == player || 
+        empty_space == opponent || 
+        empty_space == DEFAULT_PLAYER_CHAR || 
+        empty_space == DEFAULT_OPPONENT_CHAR) 
+
         empty_space = DEFAULT_EMPTY_CHAR;
+}
 
-        board.fill(empty_space);
+bool player_move(unsigned move, GAMESTATE& game)
+{
+    //only advanced if space is valid; otherwise continue
+    if (game.board.at(move) == game.empty_space)
+    {
+        game.board.at(move) = game.player_char;
+        game.turns_left--;
+        game.is_player_turn = false;
+        return true;
+        //if (server_fd)   send_to_server(player_char + ": <Input: " + std::to_string(input) + ">\n");
+        //is_player_turn = false;
+        //turns_left--;
+    }
+    std::cout << " << Invalid: Pick a new space >>\n";
+    return false;
+}
 
-        std::cout << "Welcome, player " << player_char << "!\n" << std::endl;
+bool robot_move(unsigned move, GAMESTATE& game)
+{
+    if (game.board.at(move) == game.empty_space) 
+    {
+        std::cout << "robot made a valid move at " << move << '\n';
+        game.board.at(move) = game.opp_char;
+        game.is_player_turn = true;
+        return true;
+        //if (server_fd)   send_to_server(opp_char + ": <Input: " + std::to_string(i) + ">\n");
+        //is_player_turn = true;
+        //break;
     }
 
-    //Constructor for player to define board decorations, each decoration length 1.
-    TTT(const std::string player, const std::string opponent, const std::string empty_space_) 
-        : points(0), server_fd(0) //0 is considered invalid
+    return false;
+}
+
+void print_board(GAMESTATE& game)//(bool send_board = false)
+{
+    std::string out{""};
+
+    out = "-------------\n";
+    for(unsigned int i = 0; i < 9; i += 3)
     {
-        //board characters can't be empty or greater than one
-        if (player.length() > 1 or player.empty()) 
-            player_char = DEFAULT_PLAYER_CHAR;
-        else
-            player_char = player;
+        out += "| " + game.board.at(0 + i) + 
+        " | " + game.board.at(1 + i) + " | " + game.board.at(2 + i) + " |\n";
+    }
+    out += "-------------\n";
 
-        if (opponent.length() > 1 or opponent.empty()) 
-            opp_char = DEFAULT_OPPONENT_CHAR;
-        else
-            opp_char = opponent;
-        
-        if (empty_space_.length() > 1 or empty_space_.empty()) 
-            empty_space = DEFAULT_EMPTY_CHAR;
-        else
-            empty_space = empty_space_;
+    std::cout << out;
 
-        board.fill(empty_space);
+    //if (send_board && server_fd)
+        //send_to_server(out);
+}
 
-        std::cout << "Welcome, player " << player_char << "!\n" << std::endl;
+void clear_board(GAMESTATE& game) 
+{
+    //reset to defaults
+    game.board.fill(game.empty_space);
+    game.turns_left = 9;
+    //is_player_turn  = 1;
+}
+
+/*
+//establishes a connection with the server, optional
+signed int establish_connection(const std::string address)
+{
+    std::cout << "\nAttempting to connect to server...";
+
+    //creating hints
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM; //tcp
+    hints.ai_family = AF_INET;      //IPv4
+    struct addrinfo *server_addr;
+
+    //attempt to connect (DNS)
+    if (getaddrinfo(address.c_str(), TTT_SERVER_PORT, &hints, &server_addr)) 
+    {
+        std::cerr << "getaddrinfo() failed." << GETSOCKETERRNO() << "\n";
+        return ERROR;
+    } else {
+        std::cout << "success.\n";
     }
 
-    //The method to actually play the game.
-    //It will draw the board and take control until
-    //the number of turns reach 0 or the player
-    //sends EOF.
-    //Depends on engine to generate pseudo-random moves by 
-    //the opponent.
-    void game(std::default_random_engine & engine) 
+    //creating the socket
+    std::cout << "Creating socket...";
+    SOCKET server_sock;
+    server_sock = 
+    socket(server_addr->ai_family,
+            server_addr->ai_socktype, 
+            server_addr->ai_protocol);
+    if (!ISVALIDSOCKET(server_sock)) 
     {
-        //store winner character 
-        //[options: player char, opponent char, none ("")]
-        std::string winner{""};
-        std::string out{""};
+        std::cerr << "socket() failed" << GETSOCKETERRNO() << "\n";
+        return ERROR;
+    } else {
+        std::cout << "success.\n";
+    }
 
-        out = "\nNew Game!\n";
-        std::cout << "\n" << out << "\n";
-        send_to_server(out);
+    //initiate the TCP connection
+    std::cout << "Connecting...";
+    if (connect(server_sock,
+        server_addr->ai_addr, 
+        server_addr->ai_addrlen)) 
+    {
+        std::cerr << "connect() failed" << GETSOCKETERRNO() << "\n";
+        return ERROR;
+    } else {
+        std::cout << "connected\n";
+    }
+    //we no longer need this information since the connection is established
+    freeaddrinfo(server_addr);
 
-        while (turns_left > 0) 
+    //finally we have a reliable socket to send the server data
+    server_fd = server_sock;
+
+    return SUCCESS;
+}
+*/
+
+
+//-----------helper functions-----------
+/*
+//sending basic string data to server
+signed int send_to_server(const std::string msg, bool verbose = 0)
+{
+    if (verbose)
+    {
+        std::cout << "Sending " << msg.length() << " bytes of data\n" <<
+        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" << msg << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+    }
+
+    int bytes_sent = send(server_fd, msg.c_str(), msg.length(), 0);
+    return SUCCESS;
+}
+*/
+
+std::string check_for_win(GAMESTATE& game)
+{
+    auto& board = game.board;
+    const std::string empty_space = game.empty_space;
+
+    //check rows
+    for(unsigned i = 0; i < 9; i += 3) 
+    {  
+        //if even one empty space is found, then there's
+        //no point in checking the entire row;
+        //plus 3 empty spaces are technically a valid win,
+        //so we want to avoid that.
+        if (board.at(0+i) == empty_space) continue;
+
+        //check [0,1,2], [3,4,5], [6,7,8]
+        if( board.at(0+i) == board.at(1+i) && 
+        board.at(1+i) == board.at(2+i) ) 
+        return board.at(0+i);
+    }
+
+    //check columns
+    for(unsigned i = 0; i < 3; i++) 
+    {
+        if (board.at(0+i) == empty_space) continue;
+
+        //check [0,3,6], [1,4,7], [2,5,8]
+        if( board.at(0+i) == board.at(3+i) && 
+        board.at(3+i) == board.at(6+i) ) 
+        return board.at(0+i);
+    }
+
+    //check diagonal -- upper left to bottom right
+    //ex. oxx
+    //    xox
+    //    xxo
+    if( board.at(0) == board.at(4) &&
+    board.at(4) == board.at(8) &&
+    board.at(8) != empty_space) 
+    return board.at(0);
+
+    //check diagonal -- bottom left to upper right
+    //ex. xxo
+    //    xox
+    //    oxx
+    if( board.at(6) == board.at(4) && 
+    board.at(4) == board.at(2) &&
+    board.at(2) != empty_space) 
+    return board.at(6);
+
+    //no one won
+    return "";
+}
+
+unsigned convert_to_unsigned(std::string s) 
+{
+    unsigned d = 0;
+
+    for(char c: s) 
         {
-            if(is_player_turn) 
-            {
-                std::cout << valid_spaces << std::endl;
-                print_board(true);
-                std::cout<< std::endl;
-
-                //get user input; data validation already checked
-                unsigned input = valid_input();
-                //user called EOF, return to main; no points
-                if (input == EOF) 
+            if (c < '0' or c > '9')
                 {
-                    std::cin.clear();
-                    if (server_fd)   send_to_server(player_char + ": <Input: EOF>\n");
-                    return;
+                    return EOF;
                 }
+            
+            d *= 10;
+            d += (c - '0');            
+        }
 
-                //only advanced if space is valid; otherwise continue
-                if (board.at(input) == empty_space) {
-                    board.at(input) = player_char;
-                    if (server_fd)   send_to_server(player_char + ": <Input: " + std::to_string(input) + ">\n");
-                    is_player_turn = false;
-                    turns_left--;
-                }
-            } else { //not player's turn
-                //get next empty spot, random assignment
-                while(1)
-                {
-                    //generates "move" in [0..8] 
-                    auto i = engine() % 9;
-                    //opponent attempts to occupy spot
-                    if (board.at(i) == empty_space) {
-                        board.at(i) = opp_char;
-                        if (server_fd)   send_to_server(opp_char + ": <Input: " + std::to_string(i) + ">\n");
-                        is_player_turn = true;
-                        break;
-                    }
-                } //end of while
-            } //end of conditional
+    return d;
+}
 
-            winner = check_for_win();
-            //no winner
-            if (winner == "") 
-            {
-                continue;
-            }
-            //if there is a winner...
-            out = "\n\n~~~Player " + 
-            (winner == player_char ? player_char : opp_char) + 
-            " won!~~~\n";
-
-            std::cout << out;
-            send_to_server(out);
-
-            print_board(true);
-            points += (winner == player_char ? 5 : 0);
-            clear_board();
-            return;
-        
-        } //end of while
-
-        //if turns reach 0, then no one wins
-        out = "It's a draw!\n";
-        std::cout << out;
-        send_to_server(out);
-        clear_board();
-        return;
-    }
-
-    //draws board to std::cout
-    void print_board(bool send_board = false)
+unsigned valid_input()
+{
+    std::string input = "";
+    unsigned dig = 0;
+    while(1) 
     {
-        std::string out{""};
+        std::cout << "Choose a space >> ";
+        std::getline(std::cin, input);
 
-        out = "-------------\n";
-        for(unsigned int i = 0; i < 9; i += 3)
+        if (std::cin.eof())
+        { 
+            std::cout << "<Input: EOF>" << "\n";
+            return EOF;
+        }
+
+        dig = convert_to_unsigned(input);
+
+        //adjust to work with board positions
+        dig -= BOARD_INDEX_OFFSET;
+
+        if (dig < 9)
+            return dig;
+        else
         {
-            out += "| " + board.at(0 + i) + 
-            " | " + board.at(1 + i) + " | " + board.at(2 + i) + " |\n";
-        }
-        out += "-------------\n";
-
-        std::cout << out;
-
-        if (send_board && server_fd)
-            send_to_server(out);
-    }
-
-    //returns the points the user accumalated 
-    //for each win
-    unsigned get_points(void) 
-    {
-        return points;
-    }
-
-    //resets the board for a new game
-    void clear_board(void) 
-    {
-        //reset to defaults
-        board.fill(empty_space);
-        turns_left      = 9;
-        is_player_turn  = 1;
-    }
-
-    //establishes a connection with the server, optional
-    signed int establish_connection(const std::string address)
-    {
-        std::cout << "\nAttempting to connect to server...";
-
-        //creating hints
-        struct addrinfo hints;
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_socktype = SOCK_STREAM; //tcp
-        hints.ai_family = AF_INET;      //IPv4
-        struct addrinfo *server_addr;
-
-        //attempt to connect (DNS)
-        if (getaddrinfo(address.c_str(), TTT_SERVER_PORT, &hints, &server_addr)) 
-        {
-            std::cerr << "getaddrinfo() failed." << GETSOCKETERRNO() << "\n";
-            return ERROR;
-        } else {
-            std::cout << "success.\n";
-        }
-
-        //creating the socket
-        std::cout << "Creating socket...";
-        SOCKET server_sock;
-        server_sock = 
-        socket(server_addr->ai_family,
-                server_addr->ai_socktype, 
-                server_addr->ai_protocol);
-        if (!ISVALIDSOCKET(server_sock)) 
-        {
-            std::cerr << "socket() failed" << GETSOCKETERRNO() << "\n";
-            return ERROR;
-        } else {
-            std::cout << "success.\n";
-        }
-
-        //initiate the TCP connection
-        std::cout << "Connecting...";
-        if (connect(server_sock,
-            server_addr->ai_addr, 
-            server_addr->ai_addrlen)) 
-        {
-            std::cerr << "connect() failed" << GETSOCKETERRNO() << "\n";
-            return ERROR;
-        } else {
-            std::cout << "connected\n";
-        }
-        //we no longer need this information since the connection is established
-        freeaddrinfo(server_addr);
-
-        //finally we have a reliable socket to send the server data
-        server_fd = server_sock;
-
-        return SUCCESS;
-    }
-
-    private:
-
-    //-----------player-----------
-    //accumalated points for each win
-    unsigned points;
-
-    //-----------board control-----------
-    std::array<std::string, 9> board;
-    bool is_player_turn     = 1;
-    unsigned int turns_left = 9;
-
-    //-----------helpers-----------
-    const std::string valid_spaces = "1 2 3\n"
-    "4 5 6\n"
-    "7 8 9\n";
-
-    //-----------board decorations-----------
-    std::string player_char;
-    std::string opp_char;
-    std::string empty_space;
-
-    //-----------networking------------------
-    SOCKET server_fd; //0 is not valid
-
-    //-----------helper functions-----------
-
-    //sending basic string data to server
-    signed int send_to_server(const std::string msg, bool verbose = 0)
-    {
-        if (verbose)
-        {
-            std::cout << "Sending " << msg.length() << " bytes of data\n" <<
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" << msg << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-        }
-
-        int bytes_sent = send(server_fd, msg.c_str(), msg.length(), 0);
-        return SUCCESS;
-    }
-
-    //check for win conditions in a traditional
-    //3x3 Tic Tac Toe game
-    std::string check_for_win()
-    {
-        //check rows
-        for(unsigned i = 0; i < 9; i += 3) 
-        {  
-            //if even one empty space is found, then there's
-            //no point in checking the entire row;
-            //plus 3 empty spaces are technically a valid win,
-            //so we want to avoid that.
-            if (board.at(0+i) == empty_space) continue;
-
-            //check [0,1,2], [3,4,5], [6,7,8]
-            if( board.at(0+i) == board.at(1+i) && 
-            board.at(1+i) == board.at(2+i) ) 
-            return board.at(0+i);
-        }
-
-        //check columns
-        for(unsigned i = 0; i < 3; i++) 
-        {
-            if (board.at(0+i) == empty_space) continue;
-
-            //check [0,3,6], [1,4,7], [2,5,8]
-            if( board.at(0+i) == board.at(3+i) && 
-            board.at(3+i) == board.at(6+i) ) 
-            return board.at(0+i);
-        }
-
-        //check diagonal -- upper left to bottom right
-        //ex. oxx
-        //    xox
-        //    xxo
-        if( board.at(0) == board.at(4) &&
-        board.at(4) == board.at(8) &&
-        board.at(8) != empty_space) 
-        return board.at(0);
-
-        //check diagonal -- bottom left to upper right
-        //ex. xxo
-        //    xox
-        //    oxx
-        if( board.at(6) == board.at(4) && 
-        board.at(4) == board.at(2) &&
-        board.at(2) != empty_space) 
-        return board.at(6);
-
-        //no one won
-        return "";
-    }
-
-    //converts string to unsigned int, data validation checked
-    unsigned convert_to_unsigned(std::string s) 
-    {
-        unsigned d = 0;
-
-        for(char c: s) 
-            {
-                if (c < '0' or c > '9')
-                    {
-                       return EOF;
-                    }
-                
-                d *= 10;
-                d += (c - '0');            
-            }
-
-        return d;
-    }
-
-    //gets user input from std::in
-    unsigned valid_input()
-    {
-        std::string input = "";
-        unsigned dig = 0;
-        while(1) 
-        {
-            std::cout << "Choose a space >> ";
-            std::getline(std::cin, input);
-
-            if (std::cin.eof())
-            { 
-                std::cout << "<Input: EOF>" << "\n";
-                return EOF;
-            }
-
-            dig = convert_to_unsigned(input);
-
-            //adjust to work with board positions
-            dig -= BOARD_INDEX_OFFSET;
-
-            if (dig < 9)
-                return dig;
-            else
-            {
-                std::cout << "Only [1...9]\n\n";
-                std::cin.clear();
-            }
+            std::cout << "Only [1...9]\n\n";
+            std::cin.clear();
         }
     }
-
-}; //end of class
+}
--- ttt_client.cpp
+++ ttt_client.cpp
@@ -1,30 +1,265 @@
+#include "helpers.hpp"
 #include <iostream>
-#include <chrono>
-#include <random>
-#include "TTT.cpp"
+
+
+// so we know how to interpret server messages (type-length-data)
+enum SERVER_TO_CLIENT_TYPES {REQUESTING_TAG = 1, REQUESTING_MOVE};
+
+// so we can send our messages to the server (type-length-data)
+enum CLIENT_TO_SERVER_TYPES {SEND_TAG = 1, SEND_MOVE, TERMINATED = 200 };
+
+// call to connect to TTT server given a ip and port.
+// exits program upon failure to create a socket or connect to server.
+SOCKET connect_to_ttt_server(std::string ip, std::string port);
+
+// to send a single move to the server
+// big-endian: function will convert the move to network order
+unsigned Send_Move(u_int32_t move, SOCKET server);
+
+// to ensure user enters a valid digit [1-9]
+unsigned convert_to_unsigned(std::string s);
+
+// get user input from stdin, continuously ask for board position
+// until int is entered, or EOF; [lower...upper] inclusive
+u_int32_t get_board_position(unsigned lower, unsigned upper);
+
 
 int main(int argc, char * argv[]) 
 {
-    //next 2 lines from https://zhang-xiao-mu.blog/2019/11/02/random-engine-in-modern-c/
-    //seed + engine necessary to produce pseudo-random numbers
-    unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    std::default_random_engine e(seed);
-    
-    TTT session("X", "O", "~");
+    // defaults for connecting to the server
+    std::string TTT_ip {"127.0.0.1"};
+    std::string TTT_port {"6767"};
 
-    //a remote connection will only be initiated if the
-    //user provides an address
-    if (argc > 1)
-        session.establish_connection(argv[1]);
+    if (argc > 1) // if other ip or port specified
+    {
+        TTT_ip = argv[1]; // should be alterantive ip
+        if(argc > 2)
+            TTT_port = argv[2]; // should be alternative port
+    }
 
-    //.game() must be provided a generator for opponent
-    session.game(e);
+    SOCKET server = connect_to_ttt_server(TTT_ip, TTT_port);
 
-    std::cout << "Your points: " << session.get_points() << std::endl;
+    unsigned move_from_player = 0;
 
-    session.game(e);
+    /*
+        There should be at most 2 sockets to listen for:
+        1. server
+        2. stdin (player)
+    */
 
-    std::cout << "Your points: " << session.get_points() << std::endl;
+   fd_set master;      // socket collection
+   fd_set reads;       // for checking conditions -- copy of master
+   FD_ZERO(&master);   // zero out
+   FD_SET(STDIN_FILENO, &master); // assumes 0 on most systems for stdin
+   FD_SET(server, &master); // add connection to server
+   SOCKET max_socket = server;
+   
+   std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~ Let's Play! ~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+   while(true)
+    {    
+        reads = master;
+        struct timeval timeout { .tv_sec = 0 , .tv_usec = 500000 /*microseconds*/ }; // 0.5 seconds
+
+        //std::cout << "Before select()\n";
+        if (select(max_socket+1, &reads, 0, 0, &timeout) < 0) 
+        {
+            std::cerr << "select() failed. " << GETSOCKETERRNO() << '\n';
+            exit(EXIT_FAILURE);
+        }
+        //std::cout << "After select()\n";
+
+        // safe exit by player
+        if (FD_ISSET(STDIN_FILENO, &reads))
+        {
+           // std::string admin {};
+           // std::cin >> admin;
+           // for administrators to safely exit
+        
+            if (std::cin.eof()) 
+            { 
+                break; // from while
+            } else {
+                std::cin.ignore(1000, '\n'); // flush any leftover input
+                std::cin.clear();
+            }
+        }
+
+        // wait until server is ready 
+        if (FD_ISSET(server, &reads)) 
+        {
+            std::cout << "server ready for data\n";
+
+            TYPE_LENGTH_DATA server_msg;
+            int bytes_received = recv(server, &server_msg, sizeof(TYPE_LENGTH_DATA), 0);
+            std::cout << "Received: " << bytes_received << " from server" << std::endl;
+
+            if (bytes_received < 1) // connection lost
+            {
+                std::cout << "Connection closed by server.\n";
+                break; // from loop, end program
+            }
+
+            // if the server is ready for a move
+            if (server_msg.type == SERVER_TO_CLIENT_TYPES::REQUESTING_MOVE)
+            {
+                char buff[256];
+                memset(buff, 0, 256); // zero out
+                // guaranteed null termination since TYPE_LENGTH_DATA::length < 256
+
+                // converting to ASCII
+                for(u_int8_t i = 0; i < server_msg.length; i++) buff[i] = char(server_msg.payload[i]);
+
+                std::cout << "Message from server:\n";
+                std::cout << buff << std::endl; // display message from server (typically TTT board)
+                std::cout << "-------------------\n";
+
+                move_from_player = get_board_position(1,9);   // then get move from player
+                if (move_from_player == EOF) break; // from loop, end program
+                if (Send_Move(move_from_player-1, server)) break; // from loop, end program
+                //user options [1...9] - 1 --> indexing [0..8]
+            }
+
+            std::cout << "finished reading from server\n";
+        }
+    } // while
+
+    std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~ Finished ~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+    std::cout << "Closing server socket...\n";
+    CLOSESOCKET(server);
+
+    std::cout << "Thank you, game finished." << std::endl;
 
     return 0;
+}
+
+SOCKET connect_to_ttt_server(std::string ip, std::string port)
+{
+    std::cout << "Configuring Tic Tac Toe remote address...\n";
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM; //tcp
+    hints.ai_family   = AF_INET;     // IPv4
+    
+    struct addrinfo *server_list;
+   
+    if (getaddrinfo(ip.c_str(), port.c_str(), &hints, &server_list)) {
+        std::cerr << "getaddrinfo() failed. " << GETSOCKETERRNO() << '\n';
+        exit(EXIT_FAILURE);
+    }
+
+    //not necessary but we print out the remote address for debugging
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(server_list->ai_addr, server_list->ai_addrlen,
+            address_buffer, sizeof(address_buffer),
+            service_buffer, sizeof(service_buffer),
+            NI_NUMERICHOST);
+    std::cout << address_buffer << ' ' << service_buffer << '\n';
+
+    //Create the socket to connect to server
+    std::cout << "Creating socket...\n";
+
+    SOCKET server;
+    server = socket(server_list->ai_family,
+                server_list->ai_socktype, 
+                server_list->ai_protocol);
+    if (!ISVALIDSOCKET(server)) 
+    {
+        std::cerr << "socket() failed. " << GETSOCKETERRNO() << '\n';
+        exit(EXIT_FAILURE);
+    }
+
+    //connect associates a socket with a remote address and intiates
+    //the TCP connection
+    std::cout << "Connecting to Tic Tac Toe server... ";
+    if (connect(server,
+                server_list->ai_addr, 
+                server_list->ai_addrlen)) 
+    {
+        std::cerr << "connect() failed. " << GETSOCKETERRNO() << '\n';
+        exit(EXIT_FAILURE);
+    }
+
+    //we no longer need this information since the connection is established
+    freeaddrinfo(server_list);
+
+    //finally ready for user input from the terminal
+    std::cout << "Connected to Tic Tac Toe server.\n";
+
+    return server;
+}
+
+u_int32_t Send_Move(u_int32_t move, SOCKET server)
+{
+    TYPE_LENGTH_DATA packet;
+    memset(&packet, 0, sizeof(packet));
+
+    // so that the client knows what the 
+    // server is requesting
+    packet.type = CLIENT_TO_SERVER_TYPES::SEND_MOVE;
+    packet.length = 1; // we only need a byte to send the move
+    packet.payload[0] = move;
+    // it's safe to cut off any data since we don't need more than a byte to send the move
+
+    std::cout << "packet\n[\n\t" << "type\tSEND_MOVE\n\t" << "length  " << std::to_string(packet.length) <<
+    "\n\tpayload " << char(packet.payload[0] + '0') << "\n]" << std::endl;
+
+    // send the data
+    int bytes_sent = send(server, &packet, sizeof(packet), 0);
+
+    if (bytes_sent < 1)
+    {
+        std::cerr << "Lost connection with server" << std::endl;
+        return EOF;
+    }
+
+    return 0; // SUCCESS
+}
+
+unsigned convert_to_unsigned(std::string s) 
+{
+    unsigned d = 0;
+
+    for(char c: s) 
+        {
+            if (c < '0' or c > '9')
+                {
+                    return EOF;
+                }
+            
+            d *= 10;
+            d += (c - '0');            
+        }
+
+    return d;
+}
+
+u_int32_t get_board_position(unsigned lower, unsigned upper)
+{
+    std::string input = "";
+    unsigned dig = 0;
+    while(1) 
+    {
+        std::cout << "Choose a space >> ";
+        std::getline(std::cin, input);
+
+        if (std::cin.eof())
+        { 
+            std::cout << "<Input: EOF>" << "\n";
+            return EOF;
+        }
+
+        dig = convert_to_unsigned(input);
+
+        if (lower <= dig and dig <= upper)
+            return dig;
+        else
+        {
+            std::cout << "Only [" << lower << "..." << upper << "]\n\n";
+            std::cin.clear();
+        }
+    }
 }
