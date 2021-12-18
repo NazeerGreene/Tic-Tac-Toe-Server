da2553df3dfb98b0b4557e83eb865df8797ec95e
Nazeer Greene <58005647+NazeerGreene@users.noreply.github.com>
2021-12-17 17:46:21 -0800

    Client code
    Functional. May add another branch to the message types for accepting a new request to move.

---
+++ TTT_Client/TTT_Client/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TTT_Client
+//
+//  Created by Nazeer Greene on 12/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
